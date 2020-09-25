6b4b9b153b69accbd0fad94f9bbe5e455a043987
abdelhaq <m.eng.abdelhaq@gmail.com>
2020-09-25 13:26:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
 import React from "react";
-
+import {BrowserRoute as Router,Switcher,Route}  from  'react-router-dom';
+// pages
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
 export default function App() {
-  return <h1>hello from app component</h1>;
+  return(
+    <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/cart">
+        <Cart />
+      </Route>
+      <Route path="/checkout">
+        <Checkout />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/products">
+        <Products />
+      </Route>
+      <Route
+        path="/products/:id"
+        children={<ProductDetails></ProductDetails>}
+      ></Route>
+
+      <Route path="*">
+        <Error />
+      </Route>
+    </Switch>
+  </Router>
+  );
 }
